4708d807d3bb658cc8b7fe066d52d4ce21122571
Farooq Qaiser <farooq.qaiser@shopify.com>
2022-06-30 15:06:16 -0400

    add offset commit check

--- src/test/scala/GracefulShutdownAppTest.scala
+++ src/test/scala/GracefulShutdownAppTest.scala
@@ -3,8 +3,10 @@ import fs2.Stream
 import fs2.kafka._
 import io.github.embeddedkafka.{EmbeddedKafka, EmbeddedKafkaConfig}
 import munit.CatsEffectSuite
+import org.apache.kafka.common.TopicPartition
 
 import scala.concurrent.duration.{Duration, DurationInt}
+import scala.jdk.CollectionConverters.MapHasAsScala
 
 class GracefulShutdownAppTest extends CatsEffectSuite with EmbeddedKafka {
 
@@ -53,7 +55,19 @@ class GracefulShutdownAppTest extends CatsEffectSuite with EmbeddedKafka {
         _ <- IO(println("Cancelling task!"))
         _ <- fib2.cancel
         _ <- fib1.cancel
-      } yield ()
+        committedOffset <- IO.fromTry(
+          withAdminClient(
+            _.listConsumerGroupOffsets(_groupId)
+              .partitionsToOffsetAndMetadata()
+              .get()
+              .asScala
+              .map { case (tp, offsetResult) => tp -> offsetResult.offset() }
+              .toMap
+          )
+        )
+      } yield {
+        assertEquals(committedOffset, Map(new TopicPartition(_topic, 0) -> 9L))
+      }
     }
   }
 
